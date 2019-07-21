51179708cad78c417fcc801b54aece979ee808c8
Kazuma Takata <neilbuzztakata@gmail.com>
2019-07-22 02:44:48 +0900

    add lu solve inverse

--- src/LU/index.js
+++ src/LU/index.js
@@ -31,10 +31,73 @@ function LUDecomposition(A) {
   }
 }
 
+function LUsolve(A, b) {
+  let height = A.height
+  let y = new Array(height)
+  let x = new Array(height)
+  y[0] = b[0]
+  for (let i = 1; i < height; i++) {
+    let sum = 0
+    for (let j = 0; j < i; j++) {
+      sum += A.Value[i][j] * y[j]
+    }
+    y[i] = b[i] - sum
+  }
+
+  x[height - 1] = y[height - 1] / A.Value[height - 1][height - 1]
+
+  for (let i = height - 2; i > -1; i--) {
+    let sum = 0
+    for (let j = i + 1; j < height; j++) {
+      sum += A.Value[i][j] * x[j]
+    }
+    x[i] = (y[i] - sum) / A.Value[i][i]
+  }
+
+  return x
+}
+
+function LUInterse(A) {
+  let n = A.height
+  let invA = new Array(n)
+  for (let i = 0; i < n; i++) {
+    let b = new Array(n)
+    for (let j = 0; j < n; j++) {
+      if (i == j) {
+        b[j] = 1
+      } else {
+        b[j] = 0
+      }
+    }
+    let x = LUsolve(A, b)
+    invA[i] = x
+  }
+
+  let invA_T = new Array(n)
+  for (let i = 0; i < n; i++) {
+    invA_T[i] = new Array(n)
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      invA_T[i][j] = invA[j][i]
+    }
+  }
+
+  return invA_T
+}
+
 let mat = new Matrix2D(3, 3)
-let initValue = [[9, 3, 6], [4, 6, 1], [1, 1, 7]]
+let initValue = [[1, 0, 2], [2, -1, 3], [4, 1, 8]]
 mat.set(initValue)
 
 LUDecomposition(mat)
 
-console.log(mat)
+// console.log(mat)
+
+let x = LUsolve(mat, new Array(12, 17, 5))
+
+// console.log(x)
+
+let s = LUInterse(mat)
+console.log(s)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/Integration/trapezoidalRule.js',
+  entry: './src/LU/index.js',
   output: {
     filename: 'bundle.js'
   },
